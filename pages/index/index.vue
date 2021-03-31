b96045bd6b10db2c4626becf6a33913a20e9d584
Yu Xiao <secf4ult@outlook.com>
2021-03-31 18:34:18 +0800

    Add map component (debug)

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,29 +1,42 @@
 <template>
-	<view class="container">
-		
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
-	</view>
+  <view class="container">
+    <view class="intro">
+      <map :latitude="lat" :longitude="lon" :markers="markers"></map>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
-			}
-		},
-		methods: {
-
+export default {
+	data() {
+		return {
+			lat: '',
+       lon: '',
+       covers: [
+       // format
+       // {
+       //   latitude: 0,
+       //   longitude: 0,
+       //   iconPath: ''
+       // },
+       ]
 		}
+	},
+   onLoad() {
+     plus.geolocation.getCurrentPosition((p) => {
+       { latitude: lat, longitude: lon} = p.coords
+     })
+   },
+	methods: {
+
 	}
+}
 </script>
 
 <style>
-	.container {
-		padding: 20px;
-		font-size: 14px;
-		line-height: 24px;
-	}
+.container {
+  padding: 20px;
+  font-size: 14px;
+  line-height: 24px;
+}
 </style>
